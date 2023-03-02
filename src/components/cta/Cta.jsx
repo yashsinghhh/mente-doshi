cc0bd3f3e97eb2c51039bdcdaa0b214a6598c645
Yash Singh <yashsingh0666@gmail.com>
2023-03-02 17:24:40 +0530

    cta-test

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './cta.css';
 
 const Cta = () => (
+  <><div className='cta_grid'>
+  
   <div className="gpt3__cta">
     <div className="gpt3__cta-content">
       <p>Request For Counselling Session.</p>
@@ -11,7 +13,19 @@ const Cta = () => (
     <div className="gpt3__cta-btn">
       <button type="button">Get Started</button>
     </div>
-  </div>
+
+  </div><div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request For Counselling Session.</p>
+        <h3>Register Today & get the help you need to heal.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+
+    </div>
+    </div>
+    </>
 );
 
 export default Cta;
