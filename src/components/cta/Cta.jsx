1ff1bc07f92801e3b422f05164471c3f05985569
Yash Singh <yashsingh0666@gmail.com>
2023-04-04 07:48:44 +0530

    login

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -15,13 +15,13 @@ const Cta = () => (
     </div>
 
   </div><div className="gpt3__cta">
-      <div className="gpt3__cta-content">
+      {/* <div className="gpt3__cta-content">
         <p>Request For Counselling Session.</p>
         <h3>Register Today & get the help you need to heal.</h3>
       </div>
       <div className="gpt3__cta-btn">
         <button type="button">Get Started</button>
-      </div>
+      </div> */}
 
     </div>
     </div>
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -5,9 +5,7 @@ import './possibility.css';
 const Possibility = () => {
   return (
     <div className='parent-main'>
-      <div className='main-body'>
-        <p> wefuiwrgfg e rg f reufgrugirhiv vgwierv </p>
-      </div>
+      
     </div>
     
   )
